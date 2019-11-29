1b1aebc79f41ffb46d7bc3539ba2ad04997da0a6
Web Hare <artur.zajaczkowski@icloud.com>
2019-11-29 15:29:13 +0100

    Add coments

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector('canvas');
+// Przypisnie kontekstu graficznego-nowe właściwości i metody, do ktorych odwołujemy się za pomocą obiektu CTX
 const ctx = canvas.getContext('2d');
 // context - pobieranie metod 2d do obiektu kontekstu
 
@@ -11,7 +12,11 @@ const canHei = canvas.height;
 
 function table() {
   ctx.fillStyle = 'royalblue';
-  ctx.fillRect(0, 0, 1000, 500)
+  // ustawiamy kolor na royalblue
+  ctx.fillRect(0, 0, canWid, canHei)
+  // Metoda fillRect wymaga podania 4 argumentów. Pierwsze dwa to parametry startowe na osi x i y. Drugie dwa to szerokść (oś x) i wysokośći (oś y).
+  // Lewy góry róg to parametr (0,0) a szerokość w tym przypadku 1000px (taką ma zmienna canWid) i wysokość 500px (bo taką ma zmienna canHei)
   // linie w środku
+
 }
 table()
